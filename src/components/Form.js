d6e703a3b83dd9d986e5c11b523cc7fcc103b967
Laura Richer <lauraricher@gmail.com>
2022-05-05 11:35:56 +0100

    form flow

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,51 +3,39 @@ import questions from '../json/questions.json';
 
 import Button from '../atoms/Button.js';
 
+const getQuestion = (questionId) => {
+  return questions.find(question => question.id === questionId);
+}
+
+const getNextQuestionId = (question, choiceId) => {
+  return question.choices.find(choice => choice.id === choiceId).next_question;
+}
+
 const Form = () => {
   const [questionId, setQuestionId] = useState(1);
-  const [question, setQuestion] = useState(questions[0]);
   const [choiceId, setChoiceId] = useState(1);
-  const [nextQuestionId, setNextQuestionId] = useState();
 
-  const getQuestion = (questionId) => {
-    return questions.find(question => question.id === questionId);
-  }
-
-   const updateChoice = (choice) => {
-    setChoiceId(choice);
-  }
-
-  const getNextQuestionId = (question, choiceId) => {
-    return question.choices.find(choice => choice.id === choiceId).next_question;
-  }
+  let question = getQuestion(questionId);
+  let nextQuestionId;
 
   const handleNext = (nextQuestionId) => {
-    console.log('testing');
     setQuestionId(nextQuestionId);
+    setChoiceId(1);
     // store question id to local storage
   }
 
   useEffect(() => {
     // if local storage for question id exists set as current question id & current question
-    setQuestion(getQuestion(questionId));
-    setChoiceId(1);
-    setNextQuestionId(getNextQuestionId(question, choiceId));
-
-    console.log('updated question id')
-    console.log(questionId, 'question id');
-    console.log(choiceId, 'choice id');
-    console.log(nextQuestionId, 'next question id')
+    question = getQuestion(questionId);
+    nextQuestionId = getNextQuestionId(question, choiceId);
   }, [questionId]);
 
   useEffect(() => {
-    setNextQuestionId(getNextQuestionId(question, choiceId));
-    console.log('updated choice');
-    console.log(nextQuestionId, 'next question id');
+    nextQuestionId = getNextQuestionId(question, choiceId);
   }, [choiceId]);
 
   return (
     <div>
-      {choiceId}
       <form className="form">
         <label className="form__label">{question.label}</label>
         <fieldset id={`question-${questionId}`}>
@@ -59,8 +47,8 @@ const Form = () => {
                 type="radio"
                 value={choice.value}
                 name={`question-${questionId}`}
-                defaultChecked={choiceId === index + 1}
-                onChange={() => setChoiceId(index + 1)}
+                checked={choiceId === choice.id}
+                onChange={() => setChoiceId(choice.id)}
               />
             </div>
           )}
