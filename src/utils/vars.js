dc9d82233390e3a3bf49caed3ca2f99be8f3c94b
Laura Richer <lauraricher@gmail.com>
2022-06-08 10:38:52 +0100

    enviroment variables

--- src/utils/vars.js
+++ src/utils/vars.js
@@ -1,8 +1,7 @@
 const CLIENT_ID = '82266056cc184b0faeea709c7c852cf3';
-const REDIRECT_URI = 'http://localhost:3000';
 const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 const RESPONSE_TYPE = 'token';
 const SCOPE = 'playlist-modify-private';
 
 export const API_ENDPOINT = 'https://api.spotify.com/v1';
-export const LOGIN_URL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+export const LOGIN_URL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
