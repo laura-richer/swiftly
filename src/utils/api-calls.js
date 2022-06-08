12c439722a884ab40f2184a4df114fdac91c2ef9
Laura Richer <lauraricher@gmail.com>
2022-06-08 19:11:48 +0100

    creating headers function for api calls

--- src/utils/api-calls.js
+++ src/utils/api-calls.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 import Cookies from 'js-cookie'
 import { API_ENDPOINT } from './vars.js';
 
-const headers = { Authorization: `Bearer ${Cookies.get('accessToken')}` };
+const headers = () => {
+  return { Authorization: `Bearer ${Cookies.get('accessToken')}` }
+};
 
 export const fetchUserData = async (token) => {
   try {
     const { data } = await axios.get(`${API_ENDPOINT}/me`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+      headers: headers(),
     });
 
     return data;
@@ -21,7 +21,7 @@ export const fetchUserData = async (token) => {
 export const fetchCategoryPlaylists = async (category) => {
   try {
     const { data } = await axios.get(`${API_ENDPOINT}/browse/categories/${category}/playlists?limit=50`, {
-      headers
+      headers: headers(),
     });
 
     return data;
@@ -33,7 +33,7 @@ export const fetchCategoryPlaylists = async (category) => {
 export const fetchPlaylist = async (playlist) => {
   try {
     const { data } = await axios.get(playlist, {
-      headers
+      headers: headers(),
     });
 
     return data;
@@ -45,7 +45,7 @@ export const fetchPlaylist = async (playlist) => {
 export const getPlaylist = async (playlistId) => {
   try {
     const { data } = await axios.get(`${API_ENDPOINT}/playlists/${playlistId}`, {
-      headers
+      headers: headers(),
     });
 
     return data;
@@ -68,7 +68,7 @@ const createPlaylist = async (userId) => {
 
   try {
     const { data } = await axios.post(`${API_ENDPOINT}/users/${userId}/playlists`, requestBody, {
-      headers
+      headers: headers(),
     });
 
     return data;
@@ -87,7 +87,7 @@ const addTracksToPlaylist = async (playlistId, tracks) => {
 
   try {
     const { data } = await axios.post(`${API_ENDPOINT}/playlists/${playlistId}/tracks`, requestBody, {
-      headers
+      headers: headers(),
     });
 
     return data;
