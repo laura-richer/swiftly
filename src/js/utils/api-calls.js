729b3e84446f35bab1bd7dc63c351f8b3878ca84
Laura Richer <lauraricher@gmail.com>
2022-06-16 10:40:40 +0100

    switching to private playlist

--- src/js/utils/api-calls.js
+++ src/js/utils/api-calls.js
@@ -57,7 +57,7 @@ export const fetchSoundtrack = async playlistId => {
 const createPlaylist = async userId => {
   const requestBody = {
     name: `SwiftLY Daily soundtrack - ${new Date().toDateString()}`,
-    public: true,
+    public: false,
   };
 
   try {
--- src/js/utils/variables.js
+++ src/js/utils/variables.js
@@ -1,6 +1,6 @@
 const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 const RESPONSE_TYPE = 'token';
-const SCOPE = 'playlist-modify-public';
+const SCOPE = 'playlist-modify-private';
 
 export const API_ENDPOINT = 'https://api.spotify.com/v1';
 export const LOGIN_URL = `${AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
