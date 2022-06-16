d1c783c212c16579ec253876a49b2223e06e761b
Laura Richer <lauraricher@gmail.com>
2022-06-16 10:31:57 +0100

    public playlists

--- src/js/utils/variables.js
+++ src/js/utils/variables.js
@@ -1,6 +1,6 @@
 const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 const RESPONSE_TYPE = 'token';
-const SCOPE = 'playlist-modify-private';
+const SCOPE = 'playlist-modify-public';
 
 export const API_ENDPOINT = 'https://api.spotify.com/v1';
 export const LOGIN_URL = `${AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
