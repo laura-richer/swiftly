b9393fd61172fbb381b8977e271b0a6acbd718be
Laura Richer <user@macbook-pro.home>
2022-06-17 20:37:46 +0100

    adding footer link

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -3,7 +3,12 @@ import spotifyLogo from '../../assets/images/spotify-logo.png';
 const Footer = () => {
   return (
     <footer className="footer">
-      <a href="/" target="_blank" className="footer__link">
+      <a
+        href="https://www.spotify.com/us/"
+        target="_blank"
+        className="footer__link"
+        rel="noreferrer"
+      >
         <span>Powered by</span>
         <img src={spotifyLogo} alt="Powered by Spotify" className="footer__img" />
       </a>
