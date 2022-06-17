3c8b8e9fd558953f8cc501a994064f87c23cb3f5
Laura Richer <user@macbook-pro.home>
2022-06-17 20:35:30 +0100

    fixing footer blocking overflow

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -5,7 +5,7 @@ const Form = ({ question, activeId, onChange }) => {
   return (
     <form className="form">
       <label className="form__label">{question.label}</label>
-      <div id={`question-${question.id}`}>
+      <div id={`question-${question.id}`} className="form__group">
         {question.choices.map(choice => (
           <RadioButton
             key={choice.id}
